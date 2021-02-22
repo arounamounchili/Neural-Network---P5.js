877bb6e9cbde8b5fe0cb7b9fbe2f9195705ed192
arounamounchili <virtual.power.virtual@gmail.com>
2021-02-22 18:36:56 +0100

    init project

---
+++ Perceptron/Perceptron.js
@@ -0,0 +1,14 @@
+class Perceptron {
+
+    constructor() {
+        this.weights = [];
+
+        for (let i = 0; i < this.weights.length; i++) {
+            this.weights[i] = Math.random(-1, 1);
+        }
+    }
+
+    guess(inputs) {
+        let sum = 0;
+    }
+}
